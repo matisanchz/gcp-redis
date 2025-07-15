3609cb4055db3d6c33883e7e363a33f9e70b622c
matisanchz-jaba <matiassanchez@jaba.ai>
2025-07-15 19:00:56 -0300

    fix tuple

--- change_streams.py
+++ change_streams.py
@@ -71,19 +71,19 @@ def process_change(collection, operation_type, _id, document, updatedFields, rem
 def insert_user_document(user):
     logger.info(f"Processing INSERT user {user['_id']}")
     doc = get_user_document(user)
-    insert_user_documents(doc)
+    insert_user_documents([doc])
 
 def update_user_document(_id, user, updateFields):
     user_id = str(user["_id"])
     logger.info(f"Re-indexing document for user_id: {user_id}")
     doc = get_updated_user_document(user)
     delete_user_document(user_id)
-    insert_user_documents(doc)
+    insert_user_documents([doc])
     # If the user change organization, we must change the subtask docs metadata
     if "organizationId" in updateFields:
         doc = get_updated_subtask_organization_document(_id)
         delete_subtask_document(_id)
-        insert_athlete_subtask_documents(doc)
+        insert_athlete_subtask_documents([doc])
 
 # <----------------- USERIDENTITIES ---------------------->
 def insert_useridentities_document(useridentity):
@@ -92,7 +92,7 @@ def insert_useridentities_document(useridentity):
     # Consider the posibility that some useridentities register belongs to user != "ATHLETE"
     if doc:
         delete_user_document(useridentity['userId'])
-        insert_user_documents(doc)
+        insert_user_documents([doc])
 
 def update_useridentities_document(useridentity):
     logger.info(f"Processing UPDATE user {useridentity['userId']}")
@@ -112,26 +112,26 @@ def update_campaign_document(campaign):
     logger.info(f"Re-indexing document for campaign_id: {str(campaign['_id'])}")
     doc = get_updated_campaign_document(campaign)
     delete_campaign_document(str(campaign['_id']))
-    insert_campaign_documents(doc)
+    insert_campaign_documents([doc])
 
 # <----------------- TASKS ---------------------->
 def insert_task_document(task):
     logger.info(f"Re-indexing document for campaign_id: {str(task['campaignId'])}")
     doc = get_updated_task_document(task, True)
     delete_campaign_document(str(task['campaignId']))
-    insert_campaign_documents(doc)
+    insert_campaign_documents([doc])
 
 def update_task_document(task, updatedFields):
     logger.info(f"Re-indexing document for campaign_id: {str(task['campaignId'])}")
     doc = get_updated_task_document(task, False)
     delete_campaign_document(str(task['campaignId']))
-    insert_campaign_documents(doc)
+    insert_campaign_documents([doc])
 
     # If the campaignId was modified, we must restore 2 documents
     if "campaignId" in updatedFields:
         doc = get_updated_task_document(updatedFields, False)
         delete_campaign_document(str(updatedFields['campaignId']))
-        insert_campaign_documents(doc)
+        insert_campaign_documents([doc])
 
 def delete_task_document(task):
     update_task_document(task)
@@ -141,19 +141,19 @@ def insert_subtask_document(subtask):
     logger.info(f"Re-indexing document for user_id: {str(subtask['athleteId'])}")
     doc = get_updated_subtask_document(subtask, True)
     delete_subtask_document(str(subtask['athleteId']))
-    insert_athlete_subtask_documents(doc)
+    insert_athlete_subtask_documents([doc])
 
 def update_subtask_document(subtask, updatedFields):
     logger.info(f"Re-indexing document for user_id: {str(subtask['athleteId'])}")
     doc = get_updated_subtask_document(subtask, False)
     delete_subtask_document(str(subtask['athleteId']))
-    insert_athlete_subtask_documents(doc)
+    insert_athlete_subtask_documents([doc])
 
     # If the athleteId was modified, we must restore 2 documents
     if "athleteId" in updatedFields:
         doc = get_updated_subtask_document(updatedFields, False)
         delete_subtask_document(str(updatedFields['athleteId']))
-        insert_athlete_subtask_documents(doc)
+        insert_athlete_subtask_documents([doc])
 
 def delete_subtask_document(subtask):
     update_subtask_document(subtask)
@@ -165,6 +165,6 @@ def update_organization_event(_id, updatedFields, removedFields):
         logger.info(f"Processing UPDATE organization name: {name}")
         doc = get_updated_user_organization_document(_id, name)
         delete_user_document(doc.metadata["user_id"])
-        insert_user_documents(doc)
+        insert_user_documents([doc])
     else:
         logger.info(f"No changes need to be processed for organization: {_id}")
